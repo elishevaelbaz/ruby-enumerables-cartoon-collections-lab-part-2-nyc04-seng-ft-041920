0165ce5687692a32660787bf14d08a4fe6318d5d
Elisheva Elbaz <elishevaelbaz1@gmail.com>
2020-04-24 13:55:08 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -31,5 +31,6 @@ def find_valid_calls(planeteer_calls)
   # Return the first valid call found, or return nil if no valid calls are found
   
   planeteer_calls.find do |element|
-    valid_calls.include
+    valid_calls.includes?(element)
+  end
 end
